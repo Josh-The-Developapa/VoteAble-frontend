d6bb0f31301a11faa71422dcf6e89267bfe522eb
Joshua Mukisa <kiryowajoshua22@gmail.com>
2024-09-26 12:52:35 +0300

    Committed

--- src/components/Drop-down/DropDown.jsx
+++ src/components/Drop-down/DropDown.jsx
@@ -4,9 +4,11 @@ import { NavLink } from 'react-router-dom';
 import Context from '../../Context/Context.jsx';
 import { MdOutlineAccountCircle } from 'react-icons/md';
 import Profile from '../../assets/Profile.svg';
+import { useNavigate } from 'react-router-dom';
 
 const DropDown = () => {
   const ctx = useContext(Context);
+  const navigate = useNavigate();
 
   const formatName = (name) => {
     if (!name) return '';
@@ -21,6 +23,7 @@ const DropDown = () => {
     ['name', 'gender', 'Student_ID', 'password', 'class', 'house'].forEach(
       (item) => localStorage.removeItem(item)
     );
+    navigate('/login');
     window.location.reload();
   };
 
