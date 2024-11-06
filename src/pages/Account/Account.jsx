2479eaf34a1f431e4cd2e8551cc90b0b3f35ba6b
Joshua Mukisa <kiryowajoshua22@gmail.com>
2024-11-06 07:06:39 +0300

    Committed

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -23,7 +23,7 @@ function Account() {
           },
           body: JSON.stringify({
             Student_ID: localStorage.getItem('Student_ID'),
-            password: localStorage.getItem('password'),
+            name: localStorage.getItem('name'),
           }),
         });
 
@@ -76,16 +76,7 @@ function Account() {
         <div className="user-card card">
           <img className="profile-pic" src={Profile} alt="Profile" />
           <h2>{formatName(localStorage.getItem('name'))}</h2>
-          <div className="user-info">
-            <div className="info-item">
-              <RiGraduationCapLine />
-              <p>{localStorage.getItem('class')}</p>
-            </div>
-            <div className="info-item">
-              <FiHome />
-              <p>{localStorage.getItem('house')}</p>
-            </div>
-          </div>
+
           <div className="vote-status">
             {!HAR ? (
               <div className="vote-status">
