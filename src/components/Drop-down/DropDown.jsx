985b978c3c7e24a9c0ef4be0b022767d1a87c366
Joshua Mukisa <kiryowajoshua22@gmail.com>
2025-12-27 04:33:15 +0300

    Made some UI changes

--- src/components/Drop-down/DropDown.jsx
+++ src/components/Drop-down/DropDown.jsx
@@ -2,9 +2,9 @@ import React, { useContext } from 'react';
 import './DropDown.css';
 import { NavLink } from 'react-router-dom';
 import Context from '../../Context/Context.jsx';
-import { MdOutlineAccountCircle } from 'react-icons/md';
 import Profile from '../../assets/Profile.svg';
 import { useNavigate } from 'react-router-dom';
+import { IoClose } from 'react-icons/io5'; // Add this import
 
 const DropDown = () => {
   const ctx = useContext(Context);
@@ -33,59 +33,82 @@ const DropDown = () => {
 
   return (
     <div>
-      <div className="tsBG" onClick={handleClose}></div>
-      <div className="dropdown">
+      <div className="mobileNavOverlay" onClick={handleClose}></div>
+      <div className="mobileNavDrawer">
+        {/* Close Button */}
+        <button
+          className="drawerCloseBtn"
+          onClick={handleClose}
+          aria-label="Close menu"
+        >
+          <IoClose className="drawerCloseIcon" />
+        </button>
+
         {localStorage.getItem('name') ? (
-          <div>
-            {' '}
-            <div className="user-info">
-              <NavLink to="/account" className="nav-link">
-                <img className="dropDownAvatarImage" src={Profile} />
-                <h2 style={{ textAlign: 'center', fontFamily: 'Kumbh Sans' }}>
-                  Welcome,
-                  <br />
-                  <span
-                    className="highlight"
-                    style={{ fontFamily: 'Kumbh Sans' }}
-                  >
-                    {formatName(localStorage.getItem('name'))}
-                  </span>
-                </h2>
-              </NavLink>
-            </div>
+          <div className="drawerUserSection">
+            <NavLink
+              to="/account"
+              className="drawerProfileLink"
+              onClick={handleClose}
+            >
+              <img className="drawerAvatarImg" src={Profile} alt="Profile" />
+              <h2 className="drawerWelcomeText">
+                Welcome,
+                <br />
+                <span className="drawerUserNameHighlight">
+                  {formatName(localStorage.getItem('name'))}
+                </span>
+              </h2>
+            </NavLink>
           </div>
         ) : (
-          <NavLink to="/login">
-            <button className="dropDownPollLink" onClick={handleClose}>
-              Login
+          <div className="drawerUserSectionLoggedOut">
+            <div className="drawerProfilePlaceholder">
+              <img className="drawerAvatarImg" src={Profile} alt="Profile" />
+            </div>
+          </div>
+        )}
+
+        <nav className="drawerNavLinksContainer">
+          <NavLink to="/home" className="drawerNavBtn" onClick={handleClose}>
+            Home
+          </NavLink>
+
+          {localStorage.getItem('name') && (
+            <NavLink
+              to="/account"
+              className="drawerNavBtn"
+              onClick={handleClose}
+            >
+              Account
+            </NavLink>
+          )}
+
+          <NavLink to="/about" className="drawerNavBtn" onClick={handleClose}>
+            About Us
+          </NavLink>
+
+          <NavLink to="/team" className="drawerNavBtn" onClick={handleClose}>
+            Legacy Team
+          </NavLink>
+        </nav>
+
+        <div className="drawerActionSection">
+          {localStorage.getItem('name') ? (
+            <button className="drawerNavBtn drawerLogoutBtn" onClick={logout}>
+              Logout
             </button>
-          </NavLink>
-        )}
-        <NavLink to="/home" className="dropDownPollLink" onClick={handleClose}>
-          Home
-        </NavLink>
-        {localStorage.getItem('name') ? (
-          <NavLink
-            to="/account"
-            className="dropDownPollLink"
-            onClick={handleClose}
-          >
-            Account
-          </NavLink>
-        ) : (
-          ''
-        )}
-        <NavLink to="/about" className="dropDownPollLink" onClick={handleClose}>
-          About Us
-        </NavLink>
-        <NavLink to="/team" className="dropDownPollLink" onClick={handleClose}>
-          Legacy Team
-        </NavLink>
-        {localStorage.getItem('name') && (
-          <button className="dropDownPollLink logout" onClick={logout}>
-            Logout
-          </button>
-        )}
+          ) : (
+            <NavLink to="/login">
+              <button
+                className="drawerNavBtn drawerLoginBtn"
+                onClick={handleClose}
+              >
+                Login
+              </button>
+            </NavLink>
+          )}
+        </div>
       </div>
     </div>
   );
